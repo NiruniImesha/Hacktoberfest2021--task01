0ac97c7b0c8b39cf7d75672d6e8c5f265b1c0ddf
ashanq <79820774+ashanq@users.noreply.github.com>
2021-10-07 22:47:57 +0530

    Update fac.cpp

--- fac.cpp
+++ fac.cpp
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  int a,b,c,d,e,f;
-  printf("Enter first number : ")
-  scanf("%d",&a);
-  printf("Enter second number : ")
-  scanf("%f",&b);
+  int a,b,c,d,e;
+  
+  d=1;//declare the variabels
+  e=1;
+  
+  printf("Enter first number : ");
+  scanf("%d",&a);//Input Number
+  printf("Enter second number : ");
+  scanf("%d",&b);
 
-for(int i=1;i<=a;++i){
-   d=d*i; 
+for(int i=1;i<=a;++i){ //for loop
+   d=d*i; // calculation the amount
   }
-for(int i=i;i<=b;++i){
+for(int i=1;i<=b;++i){
    e=e*i; 
   }
  
-  print("%d\n%d",d,e);
+  printf("%d\n%d",d,e);//Display output
 }
