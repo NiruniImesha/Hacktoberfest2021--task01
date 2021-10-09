5627f663ac1726463aa71e16e670d5869c55cf24
Kumaran <sathiyavarathankumaran@gmail.com>
2021-10-09 12:09:54 +0530

    update fac.c

---
+++ fac.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int a,b,c,d = 1,e = 1,f;
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+
+for(int i=1;i<=a; i++){
+   d = d * i; 
+  }
+for(int i=1;i<=b;i++){
+   e = e * i; 
+  }
+ 
+  printf("%d\n%d",d,e);
+}
